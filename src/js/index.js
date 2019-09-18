c4b3f99eeee8875985a11ce0561d46d2fed059e9
Dmitriy Sukhov <dstujev@gmail.com>
2019-09-18 21:00:25 +0300

    damn blue button and case is ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 
+import LazyLoad from "vanilla-lazyload";
 import "./import/modernizr-custom";
 import "./import/modules";
 import "./import/components";
 
+// Check the webp support
 Modernizr.on('webp', function(result) {
   if (result) {
     document.body.classList.add('webp')
@@ -10,3 +12,6 @@ Modernizr.on('webp', function(result) {
     document.body.classList.add('no-webp')
   }
 });
+
+// Initialize lazyload
+new LazyLoad();
